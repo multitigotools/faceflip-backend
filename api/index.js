0424bd445d8dcf211c2b38008f495d3e87a17b8e
multitigotools <multitigo.tools@gmail.com>
2026-04-13 20:37:01 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
-  // Handle the browser "Pre-flight" check
+    // Handle the browser "Pre-flight" check
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
